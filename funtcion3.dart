e1d2f40e47e85cce11a6f92e48caf9d4b81b74a9
Bishara Muhammad Bishara <mbishara6@gmail.com>
2025-01-31 09:46:28 +0100

    new commits

--- funtcion3.dart
+++ funtcion3.dart
@@ -1,3 +1,13 @@
-void main(List<String>arguments){
+void main(List<String> arguments) {
+  int plus(int x) {
+    return x + 5;
+  }
 
+  print(plus(3));
+}
+
+int Function(int) twice(Function(int) f) {
+  return (int x) {
+    return f(f(x));
+  };
 }
