7cef70c1465b8eff23dd401ca533c11338d0901f
Bishara Muhammad Bishara <mbishara6@gmail.com>
2025-01-31 18:08:57 +0100

    changes

---
+++ function3.dart
@@ -0,0 +1,21 @@
+void main(List<String> arguments) {
+  int plus(int x) {
+    return x + 5;
+  }
+
+  final twicePlus = twice(plus);
+  final result = twicePlus(3);
+
+  print(result);
+}
+
+int Function(int) twice(Function(int) f) {
+  return (int x) {
+    return f(f(x));
+  };
+
+//   int minus(int x) => x - 3;
+// final twiceMinus = twice(minus);
+// print(twiceMinus(10));  // First subtract 3, then subtract 3 again: (10 - 3) - 3 = 4
+
+}
